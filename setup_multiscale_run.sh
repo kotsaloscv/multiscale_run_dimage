b9457f4a85c0c789f19311c89f9ea941a18e5f15
Christos Kotsalos <christos.kotsalos@epfl.ch>
2023-07-05 16:02:12 +0200

    Docker Image

---
+++ setup_multiscale_run.sh
@@ -0,0 +1,9 @@
+# Download multiscale run project from Google Drive and extract in notebooks folder
+
+pip install gdown
+
+gdown 1ZgdF4R2UgL_s8TK4lnb8qnSmhxex81gJ
+
+tar -xzf multiscale_run_rat_sscx_S1HL_V7.tar.gz -C notebooks
+
+rm multiscale_run*.tar.gz
